df90f4aca44c454aa4f0bfaf65a22fa90cb030a8
320271017 <naum.puroski@philips.com>
2025-02-12 16:37:28 +0100

    added multiple flavors of one tests

--- cucumber_cpp/ivusTest/Main.cpp
+++ cucumber_cpp/ivusTest/Main.cpp
@@ -5,5 +5,8 @@ int main(int argc, char** argv)
 {
     cucumber_cpp::library::Application application{};
 
+    application.CliParser().add_option("--language", *application.ProgramContext().EmplaceAt<std::string>("--language"))->required();
+    application.CliParser().add_option("--screen", *application.ProgramContext().EmplaceAt<std::string>("--screen"))->required();
+
     return application.Run(argc, argv);
 }
