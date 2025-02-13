3f28b4829458310945f4af944ffe1e8a63e221a7
320271017 <naum.puroski@philips.com>
2025-02-13 13:42:41 +0100

    fixed hooks

--- cucumber_cpp/ivusTest/Hooks.cpp
+++ cucumber_cpp/ivusTest/Hooks.cpp
@@ -13,22 +13,37 @@ HOOK_AFTER_ALL()
     std::cout << "HOOK_AFTER_ALL\n";
 }
 
-HOOK_BEFORE_SCENARIO("@scenariohook and @bats")
+HOOK_BEFORE_SCENARIO("@Boom.16:9")
 {
-    std::cout << "HOOK_BEFORE_SCENARIO\n";
+    std::cout << "HOOK_BEFORE_SCENARIO @Boom.16:9\n";
 }
 
-HOOK_AFTER_SCENARIO("@scenariohook and @bats")
+HOOK_BEFORE_SCENARIO("@Boom.5:4")
 {
-    std::cout << "HOOK_AFTER_SCENARIO\n";
+    std::cout << "HOOK_BEFORE_SCENARIO @Boom.5:4\n";
 }
 
-HOOK_BEFORE_STEP("@stephook and @bats")
+HOOK_BEFORE_SCENARIO("@Cart")
 {
-    std::cout << "HOOK_BEFORE_STEP\n";
+    std::cout << "HOOK_BEFORE_SCENARIO @Cart\n";
 }
 
-HOOK_AFTER_STEP("@stephook and @bats")
+HOOK_BEFORE_SCENARIO()
 {
-    std::cout << "HOOK_AFTER_STEP\n";
+    std::cout << "HOOK_BEFORE_SCENARIO all\n";
+}
+
+HOOK_AFTER_SCENARIO()
+{
+    std::cout << "HOOK_AFTER_SCENARIO all\n";
+}
+
+HOOK_BEFORE_STEP()
+{
+    std::cout << "\n-> HOOK_BEFORE_STEP";
+}
+
+HOOK_AFTER_STEP()
+{
+    std::cout << "\n<- HOOK_AFTER_STEP";
 }
